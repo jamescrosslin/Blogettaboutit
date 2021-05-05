9de41ec460bac1cdc447af11a9222425f6d17248
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-05 13:53:13 -0700

    changed the way dates are formatted with moment

--- models/article.js
+++ models/article.js
@@ -1,8 +1,18 @@
 "use strict";
 const { Sequelize, Model } = require("sequelize");
+const moment = require("moment");
 
 module.exports = (sequelize) => {
-  class Article extends Model {}
+  class Article extends Model {
+    publishedAt() {
+      const date = moment(this.createdAt).format("MMMM D, YYYY, h:mma");
+      return date;
+    }
+    shortDescription() {
+      const description = this.body.length > 200 ? this.body.slice(0, 200) + "..." : this.body;
+      return description;
+    }
+  }
   Article.init(
     {
       title: Sequelize.STRING,
